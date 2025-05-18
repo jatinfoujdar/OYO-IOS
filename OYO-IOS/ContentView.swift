520ffa9a957040d1ab93fd22b3da721eb9223749
Jatin Foujdar <jatin321001@gmail.com>
2025-05-18 21:44:02 +0530

    Initial Commit

---
+++ OYO-IOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OYO-IOS
+//
+//  Created by jatin foujdar on 18/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OYO-IOS/OYO_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  OYO_IOSApp.swift
+//  OYO-IOS
+//
+//  Created by jatin foujdar on 18/05/25.
+//
+
+import SwiftUI
+
+@main
+struct OYO_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
