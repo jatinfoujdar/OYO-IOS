85ded5cf3b9266a4ee730ccc7ad94d27e97226db
Jatin Foujdar <jatin321001@gmail.com>
2025-05-19 11:33:21 +0530

    search filter component commit

---
+++ OYO-IOS/Core/Explore/SearchFilterBarView.swift
@@ -0,0 +1,45 @@
+//
+//  SearchFilterBarView.swift
+//  OYO-IOS
+//
+//  Created by jatin foujdar on 19/05/25.
+//
+
+import SwiftUI
+
+struct SearchFilterBarView: View {
+    var body: some View {
+        HStack{
+            Image(systemName: "magnifyingglass")
+            
+            VStack(alignment: .leading, spacing: 2){
+                Text("Where to?")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                
+                Text("Any Week . Add Guest")
+                    .font(.caption)
+                    .foregroundStyle(.gray)
+            }
+            
+            Spacer()
+            
+            Button(action: {}, label:{
+                Image(systemName: "line.3.horizontal.decrease.circle")
+                    .foregroundStyle(.black)
+            })
+        }
+        .padding()
+        .overlay{
+            Capsule()
+                .stroke(lineWidth: 0.5)
+                .foregroundStyle(Color(.systemGray4))
+                .shadow(color: .black.opacity(0.4),radius: 2)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    SearchFilterBarView()
+}
