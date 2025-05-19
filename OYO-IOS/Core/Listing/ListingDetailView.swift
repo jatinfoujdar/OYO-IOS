0755c86ce60374861925b36e34290b68916c3c05
Jatin Foujdar <jatin321001@gmail.com>
2025-05-19 19:05:08 +0530

    listing details view commit

--- OYO-IOS/Core/Listing/ListingDetailView.swift
+++ OYO-IOS/Core/Listing/ListingDetailView.swift
@@ -43,7 +43,53 @@ struct ListingDetailView: View {
             
             Divider()
             
+            HStack{
+                VStack(alignment: .leading){
+                    Text("Entire Villa hosted by Ram Sharma")
+                        .font(.headline)
+                        .frame(width: 250, alignment: .leading)
+                    
+                    
+                    HStack{
+                        Text("4 guests - ")
+                        Text("4 bedroom - ")
+                        Text("4 beds - ")
+                        Text("3 baths ")
+                    }
+                    .font(.caption)
+                    .fontWeight(.semibold)
+                    
+                }
+                Spacer()
+                
+                Image("p1")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 64, height: 64)
+                    .clipShape(Circle())
+            }
+            .padding()
             
+            Divider()
+            
+            VStack(alignment: .leading, spacing: 16){
+                ForEach(0..<2){feature in
+                    HStack(spacing:12){
+                        Image(systemName: "medal")
+                        
+                        VStack(alignment: .leading){
+                            Text("Superhost")
+                                .font(.footnote)
+                                .fontWeight(.semibold)
+                            
+                            Text("Superhosts are experienced, highly rated, and committed to providing great stays for their guests.")
+                                .font(.caption)
+                                .foregroundStyle(.gray)
+                        }
+                    }
+                }
+            }
+            .padding(.vertical)
         }
         .ignoresSafeArea()
     }
