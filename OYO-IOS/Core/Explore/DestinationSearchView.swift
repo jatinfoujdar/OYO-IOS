48d8e33eb48c8d1731a578034b6de48cdb7d785f
Jatin Foujdar <jatin321001@gmail.com>
2025-05-20 23:32:19 +0530

    Search destinations commit

--- OYO-IOS/Core/Explore/DestinationSearchView.swift
+++ OYO-IOS/Core/Explore/DestinationSearchView.swift
@@ -9,18 +9,41 @@ import SwiftUI
 
 struct DestinationSearchView: View {
     @Binding var show : Bool
+    @State private var destination = ""
+    
     var body: some View {
         VStack{
             Button{
-                show.toggle()
+                withAnimation(.snappy){
+                    show.toggle()
+                }
             }label: {
                 Image(systemName: "xmark.circle")
                     .imageScale(.large)
                     .foregroundStyle(.black)
             }
-            
-            Text("Where to?")
+            VStack(alignment: .leading){
+                Text("Where to?")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                
+                HStack{
+                    Image(systemName: "magnifyingglass")
+                        .imageScale(.small)
+                    
+                    TextField("Search destinations", text: $destination)
+                        .font(.subheadline)
+                }
+                .frame(height: 44)
+                .padding(.horizontal)
+                .overlay{
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(lineWidth: 1.0)
+                        .foregroundStyle(Color(.systemGray4))
+                }
+            }
         }
+        .padding()
     }
 }
 
