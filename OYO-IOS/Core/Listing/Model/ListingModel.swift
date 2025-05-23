54df42623ee7c38cdcc5261e8c9b5f4ede872511
Jatin Foujdar <jatin321001@gmail.com>
2025-05-23 12:00:10 +0530

    enum case commit

--- OYO-IOS/Core/Listing/Model/ListingModel.swift
+++ OYO-IOS/Core/Listing/Model/ListingModel.swift
@@ -29,13 +29,49 @@ enum ListingFeatures: Int, Codable, Identifiable, Hashable{
     case superHost
 }
 
-enum ListingAmenities: Int , Identifiable, Hashable, Codable{
-    var id: Int {return self.rawValue}
-    
+enum ListingAmenities: Int, Identifiable, Hashable, Codable {
+    var id: Int { return self.rawValue }
+
     case pool
     case wifi
     case kitchen
     case laundry
     case tv
     case balcony
+
+    var title: String {
+        switch self {
+        case .pool:
+            return "Pool"
+        case .wifi:
+            return "Wi-Fi"
+        case .kitchen:
+            return "Kitchen"
+        case .laundry:
+            return "Laundry"
+        case .tv:
+            return "TV"
+        case .balcony:
+            return "Balcony"
+        }
+    }
+    
+    
+    var imageName: String {
+        switch self {
+        case .pool:
+            return "figure.pool.swim"
+        case .wifi:
+            return "wifi"
+        case .kitchen:
+            return "fork.knife"
+        case .laundry:
+            return "washer"
+        case .tv:
+            return "tv"
+        case .balcony:
+            return "building"
+        }
+    }
 }
+
