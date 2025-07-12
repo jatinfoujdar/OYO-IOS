040715de9deb760958dad385f0eb2079c9609d55
Jatin Foujdar <jatin321001@gmail.com>
2025-07-12 23:32:27 +0530

    assets commit

--- OYO-IOS/Core/Tabbar/MainTabView.swift
+++ OYO-IOS/Core/Tabbar/MainTabView.swift
@@ -4,40 +4,37 @@
 //
 //  Created by jatin foujdar on 22/05/25.
 //
-
 import SwiftUI
 
 struct MainTabView: View {
-    @AppStorage("selectTab") var selectTab : Tab = .chat
+    @AppStorage("selectTab") var selectTab: Tab = .chat
+    
+   
     var body: some View {
-        ZStack{
-            Group{
-                switch selectTab {
-                case .chat:
-                    ExploreView()
-                    
-                case .search:
-                    WishListView()
-                    
-                case .timer:
-                    GlassmorphismView()
-           
-                case .bell:
-                    Text("Bell")
-                    
-                case .user:
-                    ProfileView()
-                }
+        ZStack {
+            TabView(selection: $selectTab) {
+                ExploreView()
+                    .tag(Tab.chat)
+                WishListView()
+                    .tag(Tab.search)
+                GlassmorphismView()
+                    .tag(Tab.timer)
+                Text("Bell")
+                    .tag(Tab.bell)
+                ProfileView()
+                    .tag(Tab.user)
             }
-            .safeAreaInset(edge: .bottom){
+            .tabViewStyle(.page(indexDisplayMode: .never))
+            .animation(.easeInOut, value: selectTab)
+            .safeAreaInset(edge: .bottom) {
                 Color.clear.frame(height: 80)
             }
-            
+
             TapBarView()
         }
-     
     }
 }
+
 
 
 #Preview {
